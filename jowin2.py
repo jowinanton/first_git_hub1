02e79d422d9678771050ff156911e02782818203
jowinanton <115615121+jowinanton@users.noreply.github.com>
2022-11-15 09:37:05 +0530

    Add files via upload

---
+++ jowin2.py
@@ -0,0 +1,16 @@
+#WAP to find the number of spaces present within the given string.
+
+# a = 'Python is a programming language'
+# new_a = a.replace('','')
+# print (a)
+# print(new_a)
+# print(f'The number of spaces present within the given sting: ',a.count(''))
+
+
+
+#WAP to change the word within a string.
+
+s = input('Enter the string:\t')
+print(s)
+old_data,new_data =input('Enter the old word'), input('Enter the new word')
+print(s.replace(old_data, new_data))
